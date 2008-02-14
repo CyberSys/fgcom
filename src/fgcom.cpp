1f8809f30834566d98d0a7e21d5e768783c3d12e
Holger Wirtz <dcoredump@gmail.com>
2008-02-14 11:39:31 +0000

    ...

--- src/fgcom.cpp
+++ src/fgcom.cpp
@@ -537,6 +537,7 @@ alarm_handler (int signal)
   /* Check every DEFAULT_ALARM_TIMER seconds if position related things should happen */
 
   /* Send our coords to the server */
+#ifdef SENDTEXT
   if (initialized == 1)
     {
       //iaxc_select_call (1);
@@ -545,6 +546,7 @@ alarm_handler (int signal)
       iaxc_send_text (tmp);
       iaxc_dump_call ();
     }
+#endif
 
   if (check_special_frq (selected_frequency))
     {
