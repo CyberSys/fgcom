3275724f0683edf9cdedf87958f2eff4a6cccbf0
Holger Wirtz <dcoredump@gmail.com>
2009-01-09 21:03:51 +0000

    I forgot to add the needed files for playing sound from a file. This functions are borowed from stresstest from iaxclient.

---
+++ src/oggfile.h
@@ -0,0 +1,42 @@
+/*
+ * fgcom: a real radio VoIP client for FlightGear based on iaxclient
+ *
+ * Copyrights:
+ * Copyright (C) 2006-2008 Holger Wirtz   <dcoredump@gmail.com>
+ *                                        <wirtz@parasitstudio.de>
+ * Copyright (C) 2008,2009 Holger Wirtz   <dcoredump@gmail.com>
+ *                                        <wirtz@parasitstudio.de>
+ *                         Charles Ingels <charles@maisonblv.net>
+ *
+ * This program may be modified and distributed under the
+ * terms of the GNU General Public License. You should have received
+ * a copy of the GNU General Public License along with this
+ * program; if not, write to the Free Software Foundation, Inc.
+ * 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * This file ist partially copied from iaxclient/stresstest/file.[ch]. The
+ * original file can be found with
+ * "svn co https://iaxclient.svn.sourceforge.net/svnroot/iaxclient iaxclient"
+ */
+
+#ifndef __OGGFILE_H__
+#define __OGGFILE_H__
+
+#ifdef WIN32
+#include <windows.h>
+#else
+#include <time.h>
+#endif
+
+#include <ogg/ogg.h>
+
+static const int SPEEX_FRAME_DURATION = 20;
+static const int SPEEX_SAMPLING_RATE = 8000;
+
+int load_ogg_file(const char *filename);
+
+ogg_packet * get_next_audio_op(struct timeval now);
+
+int audio_is_eos();
+
+#endif
